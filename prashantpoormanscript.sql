55f3ca0803ce6e1afbc806bf0b726012152ba697
fatdba <34828819+fatdba@users.noreply.github.com>
2021-12-09 10:38:46 +0530

    Create prashantpoormanscript.sql
    The script is pretty basic but very handy, quick and gives you all sort of details, and I’ve played with the LAST_CALL_ET from V$SESSION to get the overall time (RUNNING_SINCE) the SQL is there in the database, rest all you can add, alter as per your need. So this is what it is …

---
+++ prashantpoormanscript.sql
@@ -0,0 +1,47 @@
+--------------------------------------------------------------------------------------------------------------------
+-- File name:   prashantpoormanscript.sql
+-- Version:     V1.1 (12-08-2021) Simple View
+-- Purpose:     This script can be used on any Oracle DB to know what all running and for how long and waiting
+--              Also provides details on SQL and SESSION level. 
+-- Author:      Prashant Dixit The Fatdba www.fatdba.com
+--------------------------------------------------------------------------------------------------------------------
+set linesize 400 pagesize 400
+select
+x.inst_id
+,x.sid
+,x.serial#
+,x.username
+,x.sql_id
+,plan_hash_value
+,sqlarea.DISK_READS
+,sqlarea.BUFFER_GETS
+,sqlarea.ROWS_PROCESSED
+,x.event
+,x.osuser
+,x.status
+,x.BLOCKING_SESSION_STATUS
+,x.BLOCKING_INSTANCE
+,x.BLOCKING_SESSION
+,x.process
+,x.machine
+,x.program
+,x.module
+,x.action
+,TO_CHAR(x.LOGON_TIME, 'MM-DD-YYYY HH24:MI:SS') logontime
+,x.LAST_CALL_ET
+,x.SECONDS_IN_WAIT
+,x.state
+,sql_text,
+ltrim(to_char(floor(x.LAST_CALL_ET/3600), '09')) || ':'
+ || ltrim(to_char(floor(mod(x.LAST_CALL_ET, 3600)/60), '09')) || ':'
+ || ltrim(to_char(mod(x.LAST_CALL_ET, 60), '09'))    RUNNING_SINCE
+from   gv$sqlarea sqlarea
+,gv$session x
+where  x.sql_hash_value = sqlarea.hash_value
+and    x.sql_address    = sqlarea.address
+and    sql_text not like '%select x.inst_id,x.sid ,x.serial# ,x.username ,x.sql_id ,plan_hash_value ,sqlarea.DISK_READS%'
+and    x.status='ACTIVE'
+and x.USERNAME is not null
+and x.SQL_ADDRESS    = sqlarea.ADDRESS
+and x.SQL_HASH_VALUE = sqlarea.HASH_VALUE
+order by RUNNING_SINCE desc;
