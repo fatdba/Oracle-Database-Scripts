48efc49ba27a5ddcbfb6e4f1be6b0b27a1ebd378
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2021-12-10 15:04:01 +0530

    Create rac_longops.sql

---
+++ rac_longops.sql
@@ -0,0 +1,21 @@
+SET LINESIZE 200
+COLUMN sid FORMAT 9999
+COLUMN serial# FORMAT 9999999
+COLUMN machine FORMAT A30
+COLUMN progress_pct FORMAT 99999999.00
+COLUMN elapsed FORMAT A10
+COLUMN remaining FORMAT A10
+
+SELECT s.inst_id,
+       s.sid,
+       s.serial#,
+       s.username,
+       s.module,
+       ROUND(sl.elapsed_seconds/60) || ':' || MOD(sl.elapsed_seconds,60) elapsed,
+       ROUND(sl.time_remaining/60) || ':' || MOD(sl.time_remaining,60) remaining,
+       ROUND(sl.sofar/sl.totalwork*100, 2) progress_pct
+FROM   gv$session s,
+       gv$session_longops sl
+WHERE  s.sid     = sl.sid
+AND    s.inst_id = sl.inst_id
+AND    s.serial# = sl.serial#;
