913cb8e5ea980c34c0f7feb94ea54c41a4a6c4ad
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2021-12-24 13:53:33 +0530

    Create awretention.sql

---
+++ awretention.sql
@@ -0,0 +1,14 @@
+col dbid format 999999999999
+col interval_minutes format 99999 head 'INTERVAL|MINUTES'
+col retention_days format 999999 head 'RETENTION|DAYS'
+col topnsql format a20
+
+
+select
+	dbid
+	, (extract ( hour from snap_interval) * 60 )
+	  + (extract ( minute from snap_interval) ) interval_minutes
+	, extract( day from retention) retention_days
+	, topnsql
+from dba_hist_wr_control
+/
