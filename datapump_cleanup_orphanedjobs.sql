bc0ee0211d40d916f27396835406198eba62b032
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2022-02-27 10:14:27 +0530

    Create datapump_cleanup_orphanedjobs.sql

---
+++ datapump_cleanup_orphanedjobs.sql
@@ -0,0 +1,12 @@
+-- Find the orphaned Data Pump jobs:
+
+SELECT owner_name, job_name, rtrim(operation) "OPERATION",
+rtrim(job_mode) "JOB_MODE", state, attached_sessions
+FROM dba_datapump_jobs
+WHERE job_name NOT LIKE 'BIN$%' and state='NOT RUNNING'
+ORDER BY 1,2;
+
+-- Drop the tables
+
+SELECT 'drop table ' || owner_name || '.' || job_name || ';'
+FROM dba_datapump_jobs WHERE state='NOT RUNNING' and job_name NOT LIKE 'BIN$%'
