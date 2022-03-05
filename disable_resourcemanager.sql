f6b035c3c2ee0990a9c8967623dcfaaa7e485717
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2022-03-05 22:24:49 +0530

    Create disable_resourcemanager.sql

---
+++ disable_resourcemanager.sql
@@ -0,0 +1,18 @@
+set linesi 190
+set pagesi 1000
+set serveroutput on
+alter system set resource_manager_plan='' scope=both; 
+execute dbms_scheduler.set_attribute('WEEKEND_WINDOW','RESOURCE_PLAN','');
+execute dbms_scheduler.set_attribute('WEEKNIGHT_WINDOW','RESOURCE_PLAN','');
+execute dbms_scheduler.set_attribute('MONDAY_WINDOW','RESOURCE_PLAN','');
+execute dbms_scheduler.set_attribute('TUESDAY_WINDOW','RESOURCE_PLAN','');
+execute dbms_scheduler.set_attribute('WEDNESDAY_WINDOW','RESOURCE_PLAN','');
+execute dbms_scheduler.set_attribute('THURSDAY_WINDOW','RESOURCE_PLAN','');
+execute dbms_scheduler.set_attribute('FRIDAY_WINDOW','RESOURCE_PLAN','');
+execute dbms_scheduler.set_attribute('SATURDAY_WINDOW','RESOURCE_PLAN','');
+execute dbms_scheduler.set_attribute('SUNDAY_WINDOW','RESOURCE_PLAN','');
+execute dbms_scheduler.close_window('TUESDAY_WINDOW');
+execute dbms_auto_task_admin.disable( client_name => 'sql tuning advisor',operation => NULL,window_name => NULL);
+commit;
+select window_name,resource_plan from dba_scheduler_windows;
+select WINDOW_NAME ,WINDOW_ACTIVE,AUTOTASK_STATUS,OPTIMIZER_STATS,SEGMENT_ADVISOR,SQL_TUNE_ADVISOR,HEALTH_MONITOR from DBA_AUTOTASK_WINDOW_CLIENTS;
