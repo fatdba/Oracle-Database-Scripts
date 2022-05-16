ca33861f158fe86281a8ad99b5e10a408fe9082a
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2022-05-16 08:35:33 +0530

    Update tmp_stats.sql

--- Admin_General/tmp_stats.sql
+++ Admin_General/tmp_stats.sql
@@ -6,3 +6,13 @@ FROM (SELECT SUM(f.phyrds) phyrds
 FROM v$tempstat f
 )
 ;
+
+
+column name format a60
+
+select ts.name tablespace_name, tf.name, tf.bytes/(1024*1024) mb
+from v$tempfile tf, v$tablespace ts
+where tf.ts# = ts.ts# order by ts.name, tf.name
+;
+
+
