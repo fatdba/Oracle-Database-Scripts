f584f993be578572c900bbaf5222c69cdeb980a5
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2022-06-21 10:16:02 -0400

    Update asm_diskdrop.sql

--- ASM/asm_diskdrop.sql
+++ ASM/asm_diskdrop.sql
@@ -3,7 +3,7 @@
 alter diskgroup data drop disk DATA_ASM0001;
 
 -----Dropping multiple disk:
-
+--
 alter diskgroup data drop disk DATA_ASM0001,DATA_ASM00002, DATA_ASM0003 rebalance power 100;
 
 ---- Monitoring the rebalance operation:
