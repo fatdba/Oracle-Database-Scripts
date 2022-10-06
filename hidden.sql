eb2f5fa2e4308e3fd08954adb83d6031c30a1c3f
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2022-10-06 08:57:47 -0400

    Create hidden.sql

---
+++ hidden.sql
@@ -0,0 +1,6 @@
+col ParameterName for a30
+col sessval for a30
+col instval for a30
+SELECT pi.ksppinm ParameterName, pcv.ksppstvl SessVal, psv.ksppstvl InstVal
+FROM x$ksppi pi, x$ksppcv pcv, x$ksppsv psv
+WHERE pi.indx = pcv.indx AND pi.indx = psv.indx AND pi.ksppinm LIKE '_kdlu_trace_layer%'
