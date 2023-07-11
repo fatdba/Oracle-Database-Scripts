4a3dd125a936b491e4df53821f0d46565d4cce06
gitlabsdsupp <138889749+gitlabsdsupp@users.noreply.github.com>
2023-07-11 14:07:05 +0100

    Create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,20 @@
+DECLARE
+tbl_count number;
+sql_stmt long;
+
+BEGIN
+    SELECT COUNT(*) INTO tbl_count 
+    FROM dba_tables
+    WHERE owner = 'system'
+    AND table_name = 'Table1';
+
+    IF(tbl_count <= 0)
+        THEN
+        sql_stmt:='
+        CREATE TABLE Table1 (
+            c1 number(6,0),
+            c2 varchar2(10)
+        )';
+        EXECUTE IMMEDIATE sql_stmt;
+    END IF;
+END;
