2736710e59ff4d264c74fb795db9d70930b4a6fc
gitlabsdsupp <138889749+gitlabsdsupp@users.noreply.github.com>
2023-07-11 14:41:27 +0100

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,21 +1,19 @@
 DECLARE
-tbl_count number;
 sql_stmt long;
 
-
 BEGIN
-    SELECT COUNT(*) INTO tbl_count 
-    FROM dba_tables
-    WHERE owner = 'system'
-    AND table_name = 'Table1';
+    sql_stmt:='
+    CREATE TABLE T1 (
+        c1 number(6,0),
+        c2 varchar2(10)
+    )';
+    EXECUTE IMMEDIATE sql_stmt;
 
-    IF(tbl_count <= 0)
-        THEN
-        sql_stmt:='
-        CREATE TABLE Table1 (
-            c1 number(6,0),
-            c2 varchar2(10)
-        )';
-        EXECUTE IMMEDIATE sql_stmt;
-    END IF;
+    EXCEPTION
+        WHEN OTHERS THEN
+            IF SQLCODE = -955 THEN
+                NULL;
+            ELSE
+                RAISE;
+            END IF;
 END;
